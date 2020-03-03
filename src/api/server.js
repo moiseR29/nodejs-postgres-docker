9dc527ec61cf456c818810f2a46c2f827ffcdb22
Moises Rebatta <moiserebatta@gmail.com>
2020-03-03 12:16:33 -0300

    se agregaron los test

--- src/api/server.js
+++ src/api/server.js
@@ -3,13 +3,17 @@ const express = require('express');
 class Server {
   constructor({ config, router }) {
     this._config = config;
-    this._express = express();
-    this._express.use(router);
+    this._app = express();
+    this._app.use(router);
+  }
+
+  getApp() {
+    return this._app;
   }
 
   start() {
     return new Promise((resolve, reject) => {
-      const http = this._express.listen(this._config.PORT, () => {
+      const http = this._app.listen(this._config.PORT, () => {
         const { port } = http.address();
         console.log(`Application running on port ${port}`);
         resolve();
--- src/config/environments/index.js
+++ src/config/environments/index.js
@@ -1,16 +1,19 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const PRODUCTION = require("./production");
-const DEVELOPMENT = require("./develop");
-const QA = require("./qa");
+const PRODUCTION = require('./production');
+const DEVELOPMENT = require('./develop');
+const QA = require('./qa');
+const TEST = require('./test');
 const { NODE_ENV } = process.env;
 
 let currentEnv = DEVELOPMENT;
 
-if (NODE_ENV === "production") {
+if (NODE_ENV === 'production') {
   currentEnv = PRODUCTION;
-} else if (NODE_ENV === "qa") {
+} else if (NODE_ENV === 'qa') {
   currentEnv = QA;
+} else if (NODE_ENV === 'test') {
+  currentEnv = TEST;
 }
 
 module.exports = currentEnv;
